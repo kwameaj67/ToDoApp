d5c5f92ac6c5724acb38c338fa8fcce71f260fc8
Kwame Agyenim - Boateng <kwameagyenimboateng64@gmail.com>
2021-04-04 21:03:50 +0000

    creating UI for to do app

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -7,13 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource {
+   
+// we create a table
+    private let table: UITableView = {
+       let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell" )
+        return table
+    }()
+//    global variable of items in lists
+    var items = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "To do List"
+        view.addSubview(table)
+        table.dataSource =  self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem:.add, target:self, action: #selector(createListItem))
     }
-
-
+    @objc private func createListItem(){
+//        creates and opens alert
+        let alert = UIAlertController(title: "New item", message: "Enter new to do list", preferredStyle:.alert)
+//        add alert actions
+        alert.addTextField { (field) in
+            field.placeholder = "Enter item..."
+        }
+        alert.addAction(UIAlertAction(title:"Cancel", style:.cancel, handler:nil))
+        alert.addAction(UIAlertAction(title: "Done", style:.default, handler:{ [weak self] (_) in
+            if let field = alert.textFields?.first{
+                if let text = field.text, !text.isEmpty{
+                    print(text)
+                    DispatchQueue.main.async {
+                        self?.items.append(text)   // append text to the items array
+                        self?.table.reloadData()  // reload cells in table view
+                    }
+                    
+                }
+            }
+        }))
+        present(alert,animated: true)
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+    }
+     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+//    this function creates and returns a cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        we create a text label cell
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
 }
 
